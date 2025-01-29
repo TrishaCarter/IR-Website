b7d716986524925e823a957d0f40b24dcf094fa9
Calvin Idom <idomcal9@gmail.com>
2025-01-29 14:22:09 -0600

    Add dynamic link, profile, setting pages

---
+++ pages/[uid]/profile/index.js
@@ -0,0 +1,7 @@
+
+
+// React page to display a user's basic account information and stats
+
+export default function Profile() {
+    return <Text>Profile</Text>
+}
---
+++ pages/[uid]/settings/index.js
@@ -0,0 +1,9 @@
+
+
+// Page to display an account's settings / preferences
+
+import { Text } from "@mantine/core"
+
+export default function Settings() {
+    return <Text>Settings</Text>
+}
