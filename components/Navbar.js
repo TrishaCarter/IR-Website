152cba179ee0ea3e77c8d29b62f1b4832e398644
Calvin Idom <idomcal9@gmail.com>
2025-01-29 14:18:41 -0600

    Work on navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,13 @@
-import { Flex, Group, Image, Text } from "@mantine/core"
+import { Flex, Group, Image, Space, Text } from "@mantine/core"
 
 export default function Navbar() {
     return (
         <Flex justify={"space-between"} align={"center"} w={"100%"} p={"lg"}>
             <Group>
                 <Text>Logo</Text>
-                <Text>Home</Text>
+                <Space w={"lg"} />
+                <Text>Dashboard</Text>
+                <Text>Problems</Text>
             </Group>
             <Text>Account</Text>
         </Flex>
