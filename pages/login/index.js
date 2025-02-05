bbd27e66fdffacaee7376dec4261b91b4fd906de
Calvin Idom <idomcal9@gmail.com>
2025-02-04 23:51:42 -0600

    Force merge Google OAuth function

--- pages/login/index.js
+++ pages/login/index.js
@@ -61,32 +61,41 @@ export default function SignInPage() {
     }
 
     let signInGoogle = async () => {
-        console.log('Signing in with Google...');
-        await signInWithPopup(auth, googleProvider)
-            .then((result) => {
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                // const credential = GoogleAuthProvider.credentialFromResult(result);
-                // const token = credential.accessToken;
-                // // The signed-in user info.
-                // const user = result.user;
-                console.log(result);
+
+        try {
+            let gAuth = await signInWithPopup(auth, googleProvider);
+            let uid = gAuth.user.uid;
+
+            let userDoc = await getUserDoc(uid);
+            if (userDoc) {
+                console.log('User data found:', userDoc);
                 setLoginMessage('Logged in successfully. Redirecting to dashboard...');
+                loginUser(uid).then(() => {
+                    router.push('/dashboard');
+                }).catch((error) => {
+                    console.error('Error with User Cookie:', error);
+                });
+                return;
+            }
 
-                // ----------- TODO -------------
-                // Make sure to take account token here to make JWT
-                // ------------------------------
+            let userData = {
+                email: gAuth.user.email,
+                displayName: gAuth.user.displayName,
+            }
+
+            try {
+                createUserDoc(uid, userData);
+                console.log("User data created successfully.");
                 router.push('/dashboard');
-            }).catch((error) => {
-                // Handle Errors here.
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log(errorCode, errorMessage);
-                // The email of the user's account used.
-                const email = error.email;
-                // The AuthCredential type that was used.
-                const credential = GoogleAuthProvider.credentialFromError(error);
+            } catch (error) {
+                console.error('Error creating user data:', error);
+            }
 
-            });
+        } catch (error) {
+            console.log("Error with Google Sign In Popup:")
+            console.error(error);
+        }
+
     }
 
 
