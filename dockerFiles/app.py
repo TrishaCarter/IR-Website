45116293217db1f4814cf81e61674bb77ea8f946
Calvin Idom <idomcal9@gmail.com>
2025-03-28 15:14:47 -0500

    Panic

--- dockerFiles/app.py
+++ dockerFiles/app.py
@@ -16,17 +16,23 @@ def check():
         data = request.get_json()
 
         # Save the incoming request data to a file
-        repo_path = os.path.join(os.getcwd(), "optimizinator69420")
+        repo_path = os.path.join(os.getcwd(), "C_DeezNuts_Source_Code", "ir_compiler")
         with open(os.path.join(repo_path, "testing.c"), "w") as f:
             f.write(data["code"])
 
-        output = subprocess.check_output(["make"], cwd=repo_path, universal_newlines=True)
-        print(output)
-        print()
+        print(data["code"])
 
-        return jsonify({"output": output}), 200
+        # output = subprocess.check_output(["python3", "c_preprocess.py", "testing.c", '{"#1": "$1"}', "c.rbe", "1", "to>&1"], cwd=repo_path, universal_newlines=True)
+        output = subprocess.Popen(["python3", "c_preprocess.py", "testing.c", '{"#1": "$1"}', "c.rbe", "1"], cwd=repo_path, universal_newlines=True, stderr=subprocess.STDOUT)
+
+
+        print(os.listdir(repo_path))
+
+        print("here 2")
+        return jsonify({"output": "Process complete"}), 200
     except subprocess.CalledProcessError as e:
-        return jsonify({"error": str(e)}), 500
+        print("here 1")
+        return jsonify({"error": str(e)}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1738)
