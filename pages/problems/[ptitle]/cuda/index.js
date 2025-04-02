8feda60e62b51cab1a21be1fdbaae462b655258d
Calvin Idom <idomcal9@gmail.com>
2025-04-02 15:20:36 -0500

    Runs CUDA code, compilation error on backend

--- pages/problems/[ptitle]/cuda/index.js
+++ pages/problems/[ptitle]/cuda/index.js
@@ -49,7 +49,24 @@ export default function ProblemPage() {
             console.log(prob);
             setProblem(prob);
             setProbID(prob.probID)
-            setCode(prob.defaultCode);
+            // setCode(prob.defaultCode);
+            setCode(`#include <cuda_runtime.h>
+
+__global__ void multiply(int* a, int* b, int* c) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    c[i] = a[i] * b[i];
+}
+
+int main() {
+    int a[] = {1, 2, 3};
+    int b[] = {1, 2, 3};
+    int c[3];
+
+    multiply<<<1, 3>>>(a, b, c);
+
+    return 0;
+}
+`)
         })
     }, [])
 
